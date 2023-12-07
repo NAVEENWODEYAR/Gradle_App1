13e90403ea436d223684915fdeaf039f28af493f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-07 19:51:52 +0530

    initial commit

---
+++ src/main/java/com/bhas/BatchProcessingApp1Application.java
@@ -0,0 +1,16 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingApp1Application 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(BatchProcessingApp1Application.class, args);
+		System.out.println("\n First gradle-app");
+	}
+
+}
