8d5b38ee5478a071ffee201052b7fad7ddf10b84
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-07 21:54:47 +0530

    errors

--- src/main/java/com/bhas/config/BatchConfig.java
+++ src/main/java/com/bhas/config/BatchConfig.java
@@ -1,9 +1,55 @@
 package com.bhas.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.bhas.entity.BookEntity;
 
 @Configuration
 public class BatchConfig 
 {
 
+	@Bean
+	public Job bookReaderJob(JobRepository jobRepository, PlatformTransactionManager transactionManager)
+	{
+		return new JobBuilder("bookReadJob", jobRepository)
+				.incrementer(new RunIdIncrementer())
+				.start()
+				.build();
+	}
+	
+	@Bean
+	public Step chunkStep(JobRepository jobRepository, PlatformTransactionManager transactionManager)
+	{
+		return new StepBuilder("bookReaderStep",jobRepository).<BookEntity,BookEntity>chunk(10,transactionManager)
+				.reader()
+				.processor()
+				.writer()
+				.build();
+	}
+	
+	@Bean
+	@StepScope
+	public FlatFileItemReader<BookEntity> reader()
+	{
+		return new FlatFileItemReader<BookEntity>()
+				.name("bookReader")
+				.resource(new ClassPathResource(""))
+				.delimited()
+				.names(new String[] {})
+				
+				.build();
+	}
 }
